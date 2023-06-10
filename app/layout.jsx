bc1251933d1e2d57337894f97c78e737ac6023ec
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2023-06-10 13:02:27 -0400

    Update html attributes

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang='en' className='light' style={{ colorScheme: 'light' }}>
       <body className={inter.className}>
         <Providers>
           {/* Header */}
